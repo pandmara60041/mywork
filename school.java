bfa0d0dcb33a5c12ba35565ca768921102056daf
pandmara60041@gmail.com <pandmara60041@gmail.com>
2021-02-07 17:21:04 +0530

    first commit

---
+++ school.java
@@ -0,0 +1,9 @@
+class School{
+      
+       public String morningPrayer() {
+            System.out.println("morning prayer :: 10 am");
+              
+             return "prayer";
+           }
+
+}
